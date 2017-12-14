490cf47d52a4574999dbfe6333886cfd385b32aa
Joshua Ashton <jashton@punyhuman.com>
2017-12-14 22:22:38 +0000

    FOV unlimiting

--- src/game/client/crun/clientmode_sdk.cpp
+++ src/game/client/crun/clientmode_sdk.cpp
@@ -40,7 +40,7 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-ConVar fov_desired( "fov_desired", "75", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, 90.0 );
+ConVar fov_desired( "fov_desired", "90", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view." );
 ConVar default_fov( "default_fov", "90", FCVAR_CHEAT );
 
 extern bool IsInCommentaryMode( void );
